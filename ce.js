ba541946d3885ffb0b72741a8d5d172295675ac2
entcheva <christina.entcheva@gmail.com>
2017-02-07 18:10:40 -0500

    Add mouse pan controls, resize renderer with browser window

--- ce.js
+++ ce.js
@@ -6,37 +6,53 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 camera.position.z = 5;
 
+
 // geometry
 var geometry = new THREE.BoxGeometry(.5, .5, .5);
 var material = new THREE.MeshBasicMaterial({ color: 0xCCFF00, wireframe: true });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// loop
+
+// move object on keydown
+function move() {
+	document.addEventListener('keydown', (key) => {
+		if (key.which === 38) { // up
+			cube.position.y += 0.001;
+		} else if (key.which === 40) { // down
+			cube.position.y -= 0.001;
+		} else if (key.which === 39) { // right
+			cube.position.x += 0.001;
+		} else if (key.which === 37) { // left
+			cube.position.x -= 0.001;
+		}
+	})
+}
+
+// resize renderer with browser window
+function resize() {
+	window.addEventListener('resize', function() {
+		renderer.setSize(window.innerWidth, window.innerHeight)
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+	})
+}
+
+// add controls to pan with mouse
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+/****************************************************************/
+// render loop
 function render() {
 	requestAnimationFrame( render );
-  
+
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+
   move()
+	resize()
 
   renderer.render( scene, camera );
 }
 
 render();
-
-
-// move
-function move() {
-  document.addEventListener('keydown', (key) => {
-    if (key.which === 38) { // up
-      cube.position.y += 0.001;
-    } else if (key.which === 40) { // down
-      cube.position.y -= 0.001;
-    } else if (key.which === 39) { // right
-      cube.position.x += 0.001;
-    } else if (key.which === 37) { // left
-      cube.position.x -= 0.001;
-    }
-  }) // end eventlistener function
-} // end move function
