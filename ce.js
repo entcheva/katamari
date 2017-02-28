fb1278ca6833a1b2aaf205918de35e35246fe23b
entcheva <christina.entcheva@gmail.com>
2017-02-27 20:32:40 -0500

    Remove cube

--- ce.js
+++ ce.js
@@ -6,11 +6,11 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 camera.position.z = 5
 
-// cube
-var geometry = new THREE.BoxGeometry(.5, .5, .5);
-var material = new THREE.MeshBasicMaterial({ color: 0xCCFF00, wireframe: true });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// // cube
+// var geometry = new THREE.BoxGeometry(.5, .5, .5);
+// var material = new THREE.MeshBasicMaterial({ color: 0xCCFF00, wireframe: true });
+// var cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
 
 // cone
 var geometry2 = new THREE.ConeGeometry(.5, .5, .5);
@@ -27,15 +27,15 @@ document.addEventListener('keydown', keyboard, false)
 function keyboard(key) {
 	var speed = 0.1
 	if (key.which === 38) { // up
-		cube.position.z += speed;
+		cone.position.z += speed;
 		// camera.position.z += 1;
 		// camera.updateProjectionMatrix()
 	} else if (key.which === 40) { // down
-		cube.position.z -= speed;
+		cone.position.z -= speed;
 	} else if (key.which === 39) { // right
-		cube.position.x += speed;
+		cone.position.x += speed;
 	} else if (key.which === 37) { // left
-		cube.position.x -= speed;
+		cone.position.x -= speed;
 	} else if (key.which === 69) { // e
 		cone.position.z += speed;
 	} else if (key.which === 88) { // x
@@ -56,21 +56,17 @@ function resize() {
 	})
 }
 
-// // add controls to pan with mouse
-// controls = new THREE.OrbitControls(camera, renderer.domElement);
-
 /****************************************************************/
 // render loop
 function render() {
 	requestAnimationFrame( render );
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
 
 	cone.rotation.x += 0.01;
   cone.rotation.y += 0.01;
 
-	// resize()
 
   renderer.render( scene, camera );
 }
